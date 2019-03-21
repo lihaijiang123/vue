5452e8e307945b5de0f404b20de294fb769f4016
李海江 <lihaijiang@lihaijiangdeMacBook-Pro.local>
2019-03-21 11:27:02 +0800

    第一次提交

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <h1>{{ font }}</h1>
+    <div :title="titles" v-for="item in img">
+      <img :src="item.url" alt="titles">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    data() {
+      return {
+        titles: 'hi everybody',
+        font: '我是马尔扎哈',
+        img: []
+      }
+    },
+    methods: {
+      postData() {
+        this.$http({
+          method: 'post',
+          url: 'http://testjx.etlpx.com/app/index/advert',
+        }).then((res) => {
+          this.img = res['data']['data']
+        })
+      }
+    },
+    mounted() {
+      this.postData()
+    },
+  }
+
+</script>
+
+<style scoped>
+  .header {
+    background: indianred;
+    height: 300px;
+    width: 100%;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './header.vue'
+
+export default {
+  name: 'HelloWorld',
+  components: { Header },
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
